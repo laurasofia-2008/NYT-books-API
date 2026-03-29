3ffd496b0f9f0453812bfcac53ffc53bc6be2648
laurasofia-2008 <laurasofiamedina5@gmail.com>
2026-03-29 15:42:04 -0500

    fix PWA

--- script.js
+++ script.js
@@ -227,10 +227,10 @@ console.error(error);
 
 }
 
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('service-worker.js')
-    .then(() => console.log("Service Worker registrado"))
-    .catch(err => console.log("Error:", err));
 }
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/service-worker.js')
+    .then(() => console.log("Service Worker registrado"))
+    .catch(err => console.log("Error:", err));
 }
